7abf4bc94c05560dc36d42e032ea714c9dae0e05
Ezequiel Lopez <56897904+ezeloop@users.noreply.github.com>
2019-12-28 20:59:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,16 @@ $(document).keypress(function() {
     started = true;
   }
 });
+//probar si anda
+$(document).click(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
 
+//probar si anda
 $(".btn").click(function() {
 
   var userChosenColour = $(this).attr("id");
